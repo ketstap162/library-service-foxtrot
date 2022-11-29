86d485bfa5eb1cc4dbef35e8b99668d9f169eae7
Alex Parkhomenko <alex.wquels@gmail.com>
2022-11-29 17:29:56 +0200

    Update borrowing model with book & user fields, implement str method

--- borrowing/models.py
+++ borrowing/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.conf import settings
 from django.db import models
+from books.models import Book
 
 
 def calculate_expected_return_date(borrow_days):
@@ -13,12 +14,17 @@ class Borrowing(models.Model):
     BORROW_TERM = 14
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField(
-        default=calculate_expected_return_date(
-            borrow_days=BORROW_TERM
-        )
+        default=calculate_expected_return_date(borrow_days=BORROW_TERM)
     )
     actual_return_date = models.DateField(blank=True, null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowings"
+    )
 
     def __str__(self):
-        pass
+        return f"Book {self.book.title} was borrowed at: {self.borrow_date}. Return date: {self.expected_return_date}"
