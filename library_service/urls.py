8ee83519954fe5678ba83e5b6eeb86efa88fe9ba
Zakhar Podporinov <zp162162@gmail.com>
2022-11-29 19:55:31 +0200

    Update urls.py in library_service

--- library_service/urls.py
+++ library_service/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/books/", include("books.urls", namespace="books")),
+    path("api/", include("books.urls", namespace="books")),
     # path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
     path("api/users/", include("users.urls", namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
