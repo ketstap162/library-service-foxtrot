56eeaee2deff445d72270097454c6400d517e2b2
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-29 15:06:21 +0200

    Add REST_FRAMEWORK and AUTH_USER_MODEL

--- library_service/settings.py
+++ library_service/settings.py
@@ -128,3 +128,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+AUTH_USER_MODEL = "user.User"
