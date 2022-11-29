53c35ab51f339ca284daec9f24277fd9054c294a
Alex Parkhomenko <alex.wquels@gmail.com>
2022-11-29 17:50:06 +0200

    Add annotations & docstring for custom function

--- borrowing/models.py
+++ borrowing/models.py
@@ -5,7 +5,8 @@ from django.db import models
 from books.models import Book
 
 
-def calculate_expected_return_date(borrow_days):
+def calculate_expected_return_date(borrow_days: int) -> datetime:
+    """Calculates the date on which book has to be returned without any penalties"""
     current_date = datetime.date.today()
     return current_date + datetime.timedelta(days=borrow_days)
 
@@ -26,5 +27,5 @@ class Borrowing(models.Model):
         Book, on_delete=models.CASCADE, related_name="borrowings"
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Book {self.book.title} was borrowed at: {self.borrow_date}. Return date: {self.expected_return_date}"
