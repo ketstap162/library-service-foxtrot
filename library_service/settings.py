5d24e4402a11e67e3aba33905c6facb7fd7d96a5
Zakhar Podporinov <zp162162@gmail.com>
2022-11-29 19:21:08 +0200

    Update settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "library_service.urls"
