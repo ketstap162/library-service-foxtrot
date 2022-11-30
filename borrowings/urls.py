7c80c90da428d6b1befd2cafcce838ee64e15911
Alex Parkhomenko <alex.wquels@gmail.com>
2022-12-01 00:03:20 +0200

    Fix to inner routers, remove migrations from PR

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from borrowings.views import BorrowingViewSet
 
 router = routers.DefaultRouter()
-router.register("borrowings", BorrowingViewSet)
+router.register("", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
